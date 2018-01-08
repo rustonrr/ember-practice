9b5dba39bc5798ff5c0998d86494a7e375269812
Ruston Reformado <rustonreformado@gmail.com>
2018-01-08 15:49:28 -0700

    Completed ember practice

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,7 +1,19 @@
 import {test} from 'qunit';
 import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+import Service from '@ember/service';
 
-moduleForAcceptance('Acceptance | list rentals');
+let StubMapsService = Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
+moduleForAcceptance('Acceptance | list rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
 
 test('should show rentals as the home page', function (assert) {
   visit('/');
@@ -33,6 +45,14 @@ test('should list available rentals.', function (assert) {
   });
 });
 
-test('should filter the list of rentals by city.', function (assert) {});
+test('should filter the list of rentals by city.', function (assert) {
+  visit('/');
+  fillIn('.list-filter input', 'Seattle');
+  keyEvent('.list-filter input', 'keyup', 69);
+  andThen(function() {
+    assert.equal(find('.listing').length, 1, 'should show 1 listing');
+    assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
+  });
+});
 
 test('should show details for a selected rental', function (assert) {});
